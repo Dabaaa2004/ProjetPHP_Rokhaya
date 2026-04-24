9f896893ced0190a06e04ec79d284a6aed870c9a
Rokhayaa <guisserokhaya13@gmail.com>
2026-04-24 19:06:13 +0000

    mise a jour de Personne.php

--- Classes/Personne.php
+++ Classes/Personne.php
@@ -4,4 +4,35 @@ class Personne {
     private $id;
     private $nom;
     private $email;
+
+      public function __construct($id, $nom, $email) {
+         $this->id = $id;
+         $this->nom = $nom;
+         $this->email = $email;
+      }
+
+      public function getId() {
+        return $this->id;
+      }
+
+      public function getNom() {
+        return $this->nom;
+      }
+
+      public function getEmail() {
+        return $this->email;
+      }
+
+      public function setNom($nom) {
+        $this->nom = $nom;
+      }
+
+      public function setEmail($email) {
+        $this->email = $email;
+      }
+
+      public function afficherInfos() {
+        return "ID : $this->id | Nom : $this->nom | Email : $this->email";
+      }
+
 }
