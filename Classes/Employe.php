6e7ed2e6a6a93e16174314ce7d134d890144d987
Rokhayaa <guisserokhaya13@gmail.com>
2026-04-25 22:20:13 +0000

    mise a jour

---
+++ Classes/Employe.php
@@ -0,0 +1,23 @@
+<?php
+reauire_once "Utilisateur.php";
+
+class Employe extends Utilisateur {
+    private $salaire;
+
+    public function __construst($id, $nom, $email, $login, $motDePasse, $salaire) {
+        parent::__construct($id, $nom, $email, $login, $motDePasse);
+        $this->salaire = $salaire;
+    }
+
+    public function calculerSalaireAnnuel() {
+        return $this->salaire * 12;
+    }
+
+    public function afficherRole() {
+        return "Je suis un Employe";
+    }
+
+    public function afficher() {
+        echo $this->afficherInfos() . " | Salaire : $this->salaire <br>";
+    }
+}
